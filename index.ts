743e19fcf8e22ba6cc6a6f445685412cf2ecd4ac
Farooq <farooq1.khan@ryerson.ca>
2022-08-25 12:22:13 -0400

    trace

--- index.ts
+++ index.ts
@@ -41,19 +41,31 @@ console.log("Lazy First:\t", lazyFirst(() => 10,() => hand())())
 function and(a: Lazy<boolean>, b: Lazy<boolean>): Lazy<boolean> {
     return a() == false ? () => false : b 
 }
-function trace<T>(x:Lazy<T>):Lazy<T> {
-    return x
-}
+function trace<T>(x:Lazy<T>, message: string):Lazy<T> {
 
+    return () => {
+        console.log(message)
+        return x()
+    }
+}
 console.log("lazy and(f,f):\t", and(() => false, () => false)())
 console.log("lazy and(f,t):\t", and(() => false, () => true)())
 console.log("lazy and(t,f):\t", and(() => true, () => false)())
 console.log("lazy and(t,t):\t", and(() => true, () => true)())
 
+
+console.log("-- trace --")
+console.log("lazy and(f,f):\t", and(trace(() => false, "L"), trace(() => false, "R"))())
+console.log("lazy and(f,t):\t", and(trace(() => false, "L"), trace(() => true, "R"))())
+console.log("lazy and(t,f):\t", and(trace(() => true, "L"), trace(() => false, "R"))())
+console.log("lazy and(t,t):\t", and(trace(() => true, "L"), trace(() => true, "R"))())
+
+
 function or(a: Lazy<boolean>, b: Lazy<boolean>): Lazy<boolean> {
     return a() == false ? b : () => true
 }
 
+console.log("--")
 console.log("lazy or(f,f):\t", or(() => false, () => false)())
 console.log("lazy or(f,t):\t", or(() => false, () => true)())
 console.log("lazy or(t,f):\t", or(() => true, () => false)())
